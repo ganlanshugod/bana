5d8edf5d80fce917be20430cf4c5c43bf3423f41
R.Core <R.Core@RichardCore>
2016-05-18 17:00:32 +0800

    ##test

--- common-util/common-util/src/test/java/org/bana/common/util/basic/BeanToMapUtilTest.java
+++ common-util/common-util/src/test/java/org/bana/common/util/basic/BeanToMapUtilTest.java
@@ -23,7 +23,7 @@ import org.junit.Test;
 public class BeanToMapUtilTest {
 	public void test(){
 		//更新点代码
-		//ceshi1234
+		//ceshi12345
 	}
 	/**
 	 * Test method for {@link com.haier.common.util.BeanToMapUtil#convertMap(java.lang.Class, java.util.Map)}.
