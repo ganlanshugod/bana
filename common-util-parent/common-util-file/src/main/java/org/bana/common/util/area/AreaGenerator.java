edda6e54e499112db49c76c0c9555d119a6a2b74
coldwind <329496469@qq.com>
2020-06-15 15:54:07 +0800

    修改初始化数据接口

--- common-util-parent/common-util-file/src/main/java/org/bana/common/util/area/AreaGenerator.java
+++ common-util-parent/common-util-file/src/main/java/org/bana/common/util/area/AreaGenerator.java
@@ -136,6 +136,8 @@ public class AreaGenerator {
 		while(true){
 			index ++;
 			try {
+//				http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/44/4420.html
+//				http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/44/4420.html
 				document = Jsoup.connect(url).timeout(5000).get();
 				Elements select = document.select(selectStr);
 				if(select.isEmpty()) {
@@ -211,6 +213,12 @@ public class AreaGenerator {
 	private static String getSelect(String url) {
 		String parentId = url.substring(url.lastIndexOf("/")+1,url.lastIndexOf("."));
 //		parentId = StringUtils.rightPad(parentId, 6, '0');
+		if(StringUtils.equalsAny(parentId, "4419","4420","4604")){
+			return "table.towntable tr.towntr";
+		}
+		if(StringUtils.equalsAny(parentId, "4603")){
+			return "table.countytable tr.countytr";
+		}
 		int parentLength = parentId.length();
 		if(parentId.length() == 2){
 			return "table.citytable tr.citytr";
