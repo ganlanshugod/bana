07e6817cfcadd50db43248ac524465a69a906d0f
zhangzhichao <zzcggsddu@163.com>
2021-02-24 17:02:02 +0800

    身份证校验支持15位

--- common-util-parent/common-util-basic/src/main/java/org/bana/common/util/basic/IdCardUtil.java
+++ common-util-parent/common-util-basic/src/main/java/org/bana/common/util/basic/IdCardUtil.java
@@ -4,12 +4,16 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @ClassName: IdCardUtil
  * @Description: 校验身份证是否合法
  * @author Sun Chao
  */
 public class IdCardUtil {
+	private static final Logger LOG = LoggerFactory.getLogger(IdCardUtil.class);
 	/** 
 	* Description: 校验身份证是否合法
 	* @author Sun Chao   
@@ -18,6 +22,7 @@ public class IdCardUtil {
 	* @return  
 	*/ 
 	public static boolean isValid(String idNo) {
+		idNo = trans15bitTo18bit(idNo);
 		return isIdNoPattern(idNo) && isValidProvinceId(idNo.substring(0, 2)) && isValidDate(idNo.substring(6, 14))
 				&& checkIdNoLastNum(idNo);
 	}
@@ -130,4 +135,67 @@ public class IdCardUtil {
 		}
 		return true;
 	}
+	
+	/** 
+	* @Description: 15位身份证转18位
+	* @author zhangzhichao   
+	* @date 2021年2月24日 下午4:43:36 
+	* @param idStr
+	* @return  
+	*/ 
+	public static String trans15bitTo18bit(String idStr){
+		if(StringUtils.isBlank(idStr) || idStr.length()!=15) {
+			return idStr;
+		}
+		try {
+			String[] input = new String[15];
+			for (int i=0;i<idStr.length();i++) {
+				input[i] = String.valueOf(idStr.charAt(i));
+			}
+	        String[] result = new String[18];
+	        for(int i=0;i<input.length;i++){
+	            if(i<=5){
+	                result[i] = input[i];
+	            }else{
+	                result[i+2] = input[i];
+	            }
+	        }
+	        //年份最后两位小于17,年份为20XX，否则为19XX
+//	        if(Integer.valueOf(input[6])<=1&&Integer.valueOf(input[7])<=7){
+//	            result[6]="2";
+//	            result[7]="0";
+//	        }else{
+//	            result[6]="1";
+//	            result[7]="9";
+//	        }
+	        // 年份位19xx
+	        result[6]="1";
+            result[7]="9";
+	        //计算最后一位
+	        //前十七位乘以系数[7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2],
+	        int sum = 0;
+	        for(int i=0;i<17;i++){
+	            sum+= Integer.valueOf(result[i]) * power[i];
+	        }
+	        //对11求余，的余数 0 - 10 
+	        int rod = sum % 11;
+	        //所得余数映射到对应数字即可
+	        if(rod==0){ result[17] = "1";
+	        }else if(rod==1){ result[17] = "0";
+	        }else if(rod==2){ result[17] = "X";
+	        }else if(rod==3){ result[17] = "9";
+	        }else if(rod==4){ result[17] = "8";
+	        }else if(rod==5){ result[17] = "7";
+	        }else if(rod==6){ result[17] = "6";
+	        }else if(rod==7){ result[17] = "5";
+	        }else if(rod==8){ result[17] = "4";
+	        }else if(rod==9){ result[17] = "3";
+	        }else if(rod==10){ result[17] = "2";}
+
+	        return String.join("", result);
+		}catch (Exception e) {
+			LOG.error("15位转18身份证异常：", e);
+		}
+		return idStr;
+    }
 }
