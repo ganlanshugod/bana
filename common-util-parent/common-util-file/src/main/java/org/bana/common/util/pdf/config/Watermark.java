23aad431dc1bf067e8b569888c828fc13fcb26dd
Liu Wenjie <liuwenjiegod@126.com>
2021-06-30 15:23:21 +0800

    ##修改默认水印的字体

--- common-util-parent/common-util-file/src/main/java/org/bana/common/util/pdf/config/Watermark.java
+++ common-util-parent/common-util-file/src/main/java/org/bana/common/util/pdf/config/Watermark.java
@@ -1,9 +1,13 @@
 package org.bana.common.util.pdf.config;
 
+import java.io.IOException;
+
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.ColumnText;
 import com.itextpdf.text.pdf.GrayColor;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
@@ -16,13 +20,32 @@ import com.itextpdf.text.pdf.PdfWriter;
 */ 
 public class Watermark extends PdfPageEventHelper {
 
-	Font FONT = new Font(Font.FontFamily.HELVETICA, 30, Font.BOLD, new GrayColor(0.95f));
+	private static Font defaultFont = new Font(Font.FontFamily.HELVETICA, 30, Font.BOLD, new GrayColor(0.95f));
+	private Font font ;
     private String waterCont;//水印内容
     
     public Watermark() {
+    	try {
+			BaseFont bfChinese = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+			this.font = new Font(bfChinese, 30, Font.BOLD,new GrayColor(0.95f));
+		} catch (DocumentException e) {
+			e.printStackTrace();
+			this.font = defaultFont;
+		} catch (IOException e) {
+			this.font = defaultFont;
+			e.printStackTrace();
+		}
     }
     public Watermark(String waterCont) {
         this.waterCont = waterCont;
+    }
+
+    public Font getFont(){
+        return this.font;
+    }
+
+    public void setFont(Font font){
+        this.font = font;
     }
  
     @Override
@@ -31,7 +54,7 @@ public class Watermark extends PdfPageEventHelper {
             for(int j=0; j<5; j++) {
                 ColumnText.showTextAligned(writer.getDirectContentUnder(),
                         Element.ALIGN_CENTER,
-                        new Phrase(this.waterCont == null ? "HELLO WORLD" : this.waterCont, FONT),
+                        new Phrase(this.waterCont == null ? "HELLO WORLD" : this.waterCont, this.getFont()),
                         (50.5f+i*350),
                         (40.0f+j*150),
                         writer.getPageNumber() % 2 == 1 ? 45 : -45);
