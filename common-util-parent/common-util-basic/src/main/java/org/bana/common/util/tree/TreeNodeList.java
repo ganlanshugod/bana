c7a018d2fc0e51663ffb0c3ff6a93e67e6fff9e5
Liu Wenjie <liuwenjiegod@126.com>
2022-02-24 20:28:55 +0800

    ##增加treenode的循环逻辑

--- common-util-parent/common-util-basic/src/main/java/org/bana/common/util/tree/TreeNodeList.java
+++ common-util-parent/common-util-basic/src/main/java/org/bana/common/util/tree/TreeNodeList.java
@@ -10,7 +10,10 @@ package org.bana.common.util.tree;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 
 /** 
@@ -18,7 +21,7 @@ import java.util.List;
  * @Description: 树形节点的集合
  *  
  */
-public class TreeNodeList<T> implements Serializable{
+public class TreeNodeList<T> implements Collection<TreeNode<T>> , Serializable {
 
 	/** 
 	* @Fields serialVersionUID : 
@@ -38,6 +41,22 @@ public class TreeNodeList<T> implements Serializable{
 	public int size() {
 		return nodeList.size();
 	}
+
+	/**
+	 * 循环的Stream方法
+	 * @return
+	 */
+	public Stream<TreeNode<T>> stream(){
+		return nodeList.stream();
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public Stream<TreeNode<T>> parallelStream(){
+		return nodeList.parallelStream();
+	}
 	
 	/** 
 	* @Description: 返回是否为空的判断
@@ -49,7 +68,28 @@ public class TreeNodeList<T> implements Serializable{
 		return size() == 0;
 	}
 
-	/** 
+	@Override
+	public boolean contains(Object o) {
+		return false;
+	}
+
+	@Override
+	public Iterator<TreeNode<T>> iterator() {
+		return nodeList.iterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return nodeList.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return nodeList.toArray(a);
+	}
+
+
+	/**
 	* @Description: 返回指定序号的tree节点
 	* @author Liu Wenjie   
 	* @date 2015-12-2 下午3:23:01 
@@ -59,26 +99,58 @@ public class TreeNodeList<T> implements Serializable{
 	public TreeNode<T> get(int i) {
 		return nodeList.get(i);
 	}
+
 	
 	/** 
 	* @Description: 增加一个元素
 	* @author Liu Wenjie   
 	* @date 2015-12-2 下午5:43:22 
 	* @param t
-	* @return  
+	* @return
 	*/ 
-	public void add(TreeNode<T> t){
+	public boolean add(TreeNode<T> t){
 		if(t != null){
-			this.nodeList.add(t);
+			return this.nodeList.add(t);
 		}
+		return false;
 	}
-	
-	/** 
-	* @Description: 删除一个子元素
-	* @author Liu Wenjie   
-	* @date 2015-12-2 下午6:47:10 
-	* @param t  
-	*/ 
+
+	@Override
+	public boolean remove(Object o) {
+		return nodeList.remove(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return nodeList.containsAll(c);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends TreeNode<T>> c) {
+		return nodeList.addAll(c);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		return nodeList.removeAll(c);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		return nodeList.retainAll(c);
+	}
+
+	@Override
+	public void clear() {
+		nodeList.clear();
+	}
+
+	/**
+	 * @Description: 删除一个子元素
+	 * @author Liu Wenjie
+	 * @date 2015-12-2 下午6:47:10
+	 * @param t
+	 */
 	public void remove(TreeNode<T> t){
 		if(t != null){
 			this.nodeList.remove(t);
